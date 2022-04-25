dce60ca0d14903b046e71b55ef9b3d077a570c6b
levBagryansky <28lev11@gmail.com>
2022-04-26 01:02:40 +0300

    TCP bash without answer from server

--- client.c
+++ client.c
@@ -11,6 +11,7 @@
 
 int main(int argc, char** argv) {
     char buf[1024] = {0};
+    printf("TCP mode\n");
 
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(sock_fd == -1){
@@ -20,13 +21,17 @@ int main(int argc, char** argv) {
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(1e4);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_addr.s_addr = inet_addr("192.168.1.153");
+    printf("connecting..\n");
     int connect_fd = connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     if(connect_fd < 0){
         printf("connect_fd == %d\n", connect_fd);
         exit(1);
     }
+
     printf("connect_fd == %d\n", connect_fd);
+
     int n;
     while (1){
         n = read(STDIN_FILENO, buf, 256);
