e14782bad59a802861e29c30fda44a40d5a1260c
levBagryansky <28lev11@gmail.com>
2022-04-26 20:08:23 +0300

    works with TCP and UDP

--- server.c
+++ server.c
@@ -5,54 +5,97 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
 
 #define MAXLINE 1024
 
-int accepted;
+int udp_flag = 0;
+struct sockaddr_in cli_addr;
 
 //Коннектится с юзером
 int ConnectWithUser(){
     struct sockaddr_in serv_addr;
-
-    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int sock_fd;
+    if(udp_flag){
+        sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    }
+    else {
+        sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    }
     if (sock_fd == -1) {
         perror("sock_fd == -1, exit");
     }
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-//        serv_addr.sin_addr.s_addr = inet_addr("192.168.1.153");
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.1.153");
     serv_addr.sin_port = htons(1e4);
 
     bind(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
     printf("binded\n");
+    if(udp_flag){
+        char buf[MAXLINE] = {0};
+        int len = sizeof(cli_addr);
+        memset(&cli_addr, 0, sizeof(cli_addr));
+        int n = recvfrom(sock_fd, (char *) buf, MAXLINE,
+                         MSG_WAITALL, (struct sockaddr *) &cli_addr, (socklen_t *) &len);
+        write(STDOUT_FILENO, buf, MAXLINE);
+        char* str = "Got answer from server\n";
+        int sended = sendto(sock_fd, str, 24, MSG_CONFIRM, (const struct sockaddr *) &cli_addr, sizeof cli_addr);
+        if(sended == -1){
+            perror("sendto error");
+        }
+        return sock_fd;
+    }
     listen(sock_fd, 2);
     printf("listened\n");
     int acc_fd = accept(sock_fd, NULL, NULL);
     printf("accepted\n");
+
     return acc_fd;
 }
 
 //Получает сообщения и посылает их в pipe_get_fds
-int GetMessages(int pipe_write_fd, int acc_fd){
+int GetMessages(int pipe_write_fd, int acc_fd) {
     char buf[1024] = {0};
     int n;
-    while (1) {
-        n = read(acc_fd, buf, MAXLINE);
-        write(pipe_write_fd, buf, n);
-        memset(buf, 0, n);
+    if (udp_flag) {
+        int len;
+        while (1){
+            int n = recvfrom(acc_fd, (char *) buf, MAXLINE,
+                             MSG_WAITALL, (struct sockaddr *) &cli_addr, (socklen_t *) &len);
+            write(pipe_write_fd, buf, n);
+            write(STDOUT_FILENO, buf, n);
+            memset(buf, 0, n);
+        }
+    } else {
+        //TCP
+        while (1) {
+            n = read(acc_fd, buf, MAXLINE);
+            write(pipe_write_fd, buf, n);
+            char *command_str = "Command: ";
+            //write(STDOUT_FILENO, command_str, 10);
+            write(STDOUT_FILENO, buf, n);
+            memset(buf, 0, n);
+        }
     }
 }
 
 //из pipe_send_fds посылвет строки клиенту
 int SendMessages(int pipe_read_send_fd, int acc_fd){
-    char c;
-    char s[MAXLINE];
-    int txt_fd = open("txt",  O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    char str[MAXLINE];
     while (1) {
-        int n = read(pipe_read_send_fd, s, MAXLINE);
-        write(acc_fd, s, n);
-        write(STDOUT_FILENO, s, n);
-        memset(s, 0, MAXLINE);
+        int n = read(pipe_read_send_fd, str, MAXLINE);
+        if(udp_flag){
+            int sended = sendto(acc_fd, str, n, MSG_CONFIRM, (const struct sockaddr *) &cli_addr, sizeof cli_addr);
+            if(sended == -1){
+                perror("sendto error");
+            }
+        }else {
+            write(acc_fd, str, n);
+        }
+        write(STDOUT_FILENO, str, n);
+        memset(str, 0, MAXLINE);
     }
 }
 
@@ -70,7 +113,13 @@ int DoBash(int pipe_read_get_fd, int pipe_write_send_fd){
     dup2(clone_stdout, STDOUT_FILENO);
 }
 
-int main(){
+int main(int argc, char **argv){
+    if(argc == 2 && (!strcmp(argv[1], "UDP") || !strcmp(argv[1], "udp"))){
+        printf("UDP mode\n");
+        udp_flag = 1;
+    } else{
+        printf("TCP mode\n");
+    }
     int pipe_get_fds[2];
     int pipe_send_fds[2];
     if(pipe(pipe_get_fds)){
@@ -80,9 +129,9 @@ int main(){
         perror("pipe_send error");
     }
 
+    int acc_fd = ConnectWithUser();
     int forked = fork();
     if(!forked){
-        int acc_fd = ConnectWithUser();
         if(acc_fd < 0){
             perror("ConnectWithUser == -1");
             exit(1);
