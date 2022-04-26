0cc203d3b6d0b942847d1bd0b8e2ac9a338de336
levBagryansky <28lev11@gmail.com>
2022-04-26 16:39:45 +0300

    added udp

--- client.c
+++ client.c
@@ -11,34 +11,49 @@
 
 int main(int argc, char** argv) {
     char buf[MAXLINE] = {0};
-    printf("TCP mode\n");
-
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock_fd == -1){
+    if (sock_fd == -1) {
         printf("sock_fd == -1\n");
         exit(1);
     }
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(1e4);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_addr.s_addr = inet_addr("192.168.1.153");
-    printf("connecting..\n");
-    int connect_fd = connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    if(connect_fd < 0){
+
+    if(argc == 2 && (!strcmp(argv[1], "UDP") || !strcmp(argv[1], "udp"))) {
+        printf("UDP mode\n");
+
+        int n, len;
+        while (1){
+            n = read(STDIN_FILENO, buf, MAXLINE);
+            sendto(sock_fd, buf, n, MSG_CONFIRM, (const struct sockaddr *) &serv_addr, sizeof serv_addr);
+            memset(buf, 0, sizeof(buf));
+
+            n = recvfrom(sock_fd, buf, MAXLINE, MSG_WAITALL, (struct sockaddr *) &serv_addr, &len);
+            write(STDOUT_FILENO, buf, n);
+            memset(buf, 0, sizeof(buf));
+        }
+
+    } else {
+        printf("TCP mode");
+        printf("connecting..\n");
+        int connect_fd = connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+        if (connect_fd < 0) {
+            printf("connect_fd == %d\n", connect_fd);
+            return 1;
+        }
+
         printf("connect_fd == %d\n", connect_fd);
-        exit(1);
-    }
-
-    printf("connect_fd == %d\n", connect_fd);
-
-    int n;
-    while (1){
-        n = read(STDIN_FILENO, buf, 256);
-        write(sock_fd, buf, n);
-        memset(buf, 0, MAXLINE);
-        n = read(sock_fd, buf, MAXLINE);
-        printf("\n");
-        write(STDOUT_FILENO, buf, n);
+        int n;
+        while (1) {
+            n = read(STDIN_FILENO, buf, MAXLINE);
+            write(sock_fd, buf, n);
+            memset(buf, 0, MAXLINE);
+            n = read(sock_fd, buf, MAXLINE);
+            printf("\n");
+            write(STDOUT_FILENO, buf, n);
+            memset(buf, 0, MAXLINE);
+        }
     }
 }
