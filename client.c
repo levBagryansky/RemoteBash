e0df205442a458b8e1e0e155b5a037d649d59bf6
levBagryansky <28lev11@gmail.com>
2022-04-28 00:24:32 +0300

    united udp and tcp branches to a functions

--- client.c
+++ client.c
@@ -6,10 +6,14 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define MAXLINE 1024
-#define PORT 1e4
+#define MAXLINE   1024
+#define PORT      1e4
+#define EXIT_CODE -2
 
 struct sockaddr_in serv_addr;
+int udp_flag = 0;
+int len = sizeof (serv_addr);
+int CP2Server();
 
 int BroadcastFirstConnect(){
     printf("Broadcast connecting..\n");
@@ -40,84 +44,85 @@ int BroadcastFirstConnect(){
     return 0;
 }
 
-int DoUDP(char *buf){
-    printf("UDP mode\n");
+int ReadAndSend2Server(char *buf, int sock_fd){
+    int n = read(STDIN_FILENO, buf, MAXLINE);
+    int ret;
+    if(udp_flag) {
+        ret = sendto(sock_fd, buf, n, MSG_CONFIRM, (const struct sockaddr *) &serv_addr, sizeof serv_addr);
+    }
+    else{
+        ret = write(sock_fd, buf, n);
+    }
+    if (!strcmp(buf, "exit\n")) {
+        printf("Sender exits\n");
+        return EXIT_CODE;
+    }
+    memset(buf, 0, MAXLINE);
+    return ret;
+}
+
+int RcvAndWrite(char *buf, int sock_fd){
+    int n;
+    if(udp_flag){
+        n = recvfrom(sock_fd, buf, MAXLINE, MSG_WAITALL, (struct sockaddr *) &serv_addr, (socklen_t *)&len);
+    } else{
+        n = read(sock_fd, buf, MAXLINE);
+    }
+    if (!strcmp(buf, "exit\n")) {
+        printf("Recver exits\n");
+        return EXIT_CODE;
+    }
+    write(STDOUT_FILENO, buf, n);
+    memset(buf, 0, MAXLINE);
+    return n;
+}
+
+int DoCommunication(char* buf){
     int sock_fd;
-    // Creating socket file descriptor
-    if ( (sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+    if(udp_flag){
+        printf("UDP mode\n");
+        sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    } else{
+        printf("TCP mode\n");
+        sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    if (sock_fd == -1) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    int n, len;
-    char *str = "I know client datas\n\0";
-    sendto(sock_fd, str, 21, MSG_CONFIRM, (const struct sockaddr *) &serv_addr, sizeof serv_addr);
-    n = recvfrom(sock_fd, buf, MAXLINE, MSG_WAITALL, (struct sockaddr *) &serv_addr, (socklen_t *)&len);
-    write(STDOUT_FILENO, buf, n);
+    printf("connecting..\n");
+    int len = sizeof (serv_addr);
+    if(udp_flag){
+        char *str = "I know client datas\n\0";
+        sendto(sock_fd, str, 21, MSG_CONFIRM, (const struct sockaddr *) &serv_addr, sizeof serv_addr);
+        int n = recvfrom(sock_fd, buf, MAXLINE, MSG_WAITALL, (struct sockaddr *) &serv_addr, (socklen_t *)&len);
+        write(STDOUT_FILENO, buf, n);
+    } else{ //TCP
+        int connect_fd = connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+        if (connect_fd < 0) {
+            printf("connect_fd == %d\n", connect_fd);
+            return 1;
+        }
+        printf("connect_fd = %d\n", connect_fd);
+    }
+    printf("sock_fd = %d\nConnected, you can start\n", sock_fd);
+
     int forked = fork();
     if(forked) {
         while (1) {
-            n = read(STDIN_FILENO, buf, MAXLINE);
-            sendto(sock_fd, buf, n, MSG_CONFIRM, (const struct sockaddr *) &serv_addr, sizeof serv_addr);
-            if (!strcmp(buf, "exit\n")) {
-                printf("Sender exits\n");
+            if(ReadAndSend2Server(buf, sock_fd) == EXIT_CODE){
                 break;
             }
-            memset(buf, 0, MAXLINE);
         }
     } else{
         while (1) {
-            n = recvfrom(sock_fd, buf, MAXLINE, MSG_WAITALL, (struct sockaddr *) &serv_addr, (socklen_t *)&len);
-            if (!strcmp(buf, "exit\n")) {
-                printf("Recver exits\n");
+            if(RcvAndWrite(buf, sock_fd) == EXIT_CODE){
                 break;
             }
-            write(STDOUT_FILENO, buf, n);
-            memset(buf, 0, MAXLINE);
         }
-    }
-    return 0;
-}
-
-int DoTCP(char *buf){
-    printf("TCP mode\n");
-    int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_fd == -1) {
-        printf("sock_fd == -1\n");
-        exit(1);
-    }
-    printf("connecting..\n");
-    int connect_fd = connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
-    if (connect_fd < 0) {
-        printf("connect_fd == %d\n", connect_fd);
-        return 1;
     }
 
-    printf("connect_fd == %d\n", connect_fd);
-    int n;
-    int forked = fork();
-    if(forked){
-        while (1) {
-            n = read(STDIN_FILENO, buf, MAXLINE);
-            write(sock_fd, buf, n);
-            if (!strcmp(buf, "exit\n")) {
-                printf("Sender exits\n");
-                break;
-            }
-            memset(buf, 0, MAXLINE);
-        }
-    } else {
-        while (1) {
-            n = read(sock_fd, buf, MAXLINE);
-            if (!strcmp(buf, "exit\n")) {
-                printf("Recver exits\n");
-                break;
-            }
-            write(STDOUT_FILENO, buf, n);
-            memset(buf, 0, MAXLINE);
-        }
-    }
-    return 0;
 }
 
 int main(int argc, char** argv) {
@@ -127,8 +132,10 @@ int main(int argc, char** argv) {
     serv_addr.sin_port = htons(PORT);
 
     if(argc == 2 && (!strcmp(argv[1], "UDP") || !strcmp(argv[1], "udp"))) {
-        DoUDP(buf);
+        udp_flag++;
+        //DoUDP(buf);
     } else {
-        DoTCP(buf);
+        //DoTCP(buf);
     }
+    DoCommunication(buf);
 }
